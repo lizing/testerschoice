10d11c87f3c488d16ed9e0e0173e8959b8c5a48d
JinWoo Lee <lizingkr@gmail.com>
2012-03-14 00:53:53 +0900

    Modified the app under test

--- SingleActivityProviderApp/gen/com/lizing/simple/singleactivityprovider/R.java
+++ SingleActivityProviderApp/gen/com/lizing/simple/singleactivityprovider/R.java
@@ -16,14 +16,21 @@ public final class R {
     public static final class id {
         public static final int btn_add=0x7f050001;
         public static final int btn_del_all=0x7f050002;
+        public static final int btn_move=0x7f050003;
+        public static final int checkBox1=0x7f050005;
         public static final int editText1=0x7f050000;
+        public static final int radioButton1=0x7f050004;
+        public static final int textView1=0x7f050006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int second=0x7f030001;
     }
     public static final class string {
         public static final int add=0x7f040002;
         public static final int app_name=0x7f040000;
         public static final int del_all=0x7f040001;
+        public static final int hello=0x7f040004;
+        public static final int move=0x7f040003;
     }
 }
